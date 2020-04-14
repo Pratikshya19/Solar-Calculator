216a4167426a5323eac40c1219dc89c683e4224e
github username <pratikshya.pp19@gmail.com>
2020-04-14 12:28:03 -0500

    added Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+JavaScript Project: 
+
+Creating Solar Calculator that explores the features of JavaScript concepts in isolation. 
+
+A real world solar calculator that calculates the number of solar panels required based on the your monthly electricity usage, where you live, and which panels you select. 
+
---
+++ main.js
@@ -0,0 +1,88 @@
+'use strict';
+function addMonths(elem) {
+var annualUseKw=0, dailyUseKw=0, i=0, x=0;
+var months = document.getElementById(elem).getElementsByTagName('input');
+
+for(i=0; i<months.length; i++) {
+    x = Number(months[i].value);
+    annualUseKw += x;
+}
+dailyUseKw = annualUseKw/365
+console.log(`DailyUseKW = ${dailyUseKw}`)
+return dailyUseKw;
+
+}
+
+function sunHours(){
+var hrs;
+
+var theZone = document.forms.solarForm.zone.selectedIndex 
+theZone += 1;
+switch(theZone){
+    case 1:
+        hrs = 6;
+        break;
+    case 2:
+        hrs = 5.5;
+        break;
+    case 3:
+        hrs = 4.2;
+        break;
+    case 4:
+        hrs = 4.5;
+        break;
+    case 5:
+        hrs = 3.5;
+        break;
+    case 6:
+        hrs = 0;
+        break;
+}
+console.log(hrs)
+return hrs;
+}//end
+
+//finding panel choice
+function calculatePanel(){
+var userChoice = document.forms.solarForm.panel.selectedIndex
+ var panelopt = document.forms.solarForm.panel.options
+var power = panelopt[userChoice].value
+var name = panelopt[userChoice].text
+var x = [power, name]
+console.log(x)
+return x;
+}
+
+function calculateSolar(){
+var dailyUseKw = addMonths('mpc'); 
+console.log(dailyUseKw); //we know how much energy is needed
+var sunHoursPerday = sunHours();
+console.log(sunHoursPerday); //hrs of sunshine/day
+
+var minKwNeeded = dailyUseKw / sunHoursPerday
+console.log(minKwNeeded)
+var realKwNeeded = minKwNeeded * 1.25;
+console.log(realKwNeeded)
+
+var realWattNeeds = realKwNeeded * 1000;
+console.log(realWattNeeds)
+
+var panelInfo = calculatePanel();
+var panelOutput = panelInfo[0]
+var panelName = panelInfo[1];
+console.log(panelOutput)
+console.log(panelName)
+
+var panelsNeeded = Math.ceil(realWattNeeds / panelOutput);
+console.log(panelsNeeded)
+
+var feedback = "";
+feedback += "<p>Based on your avergae daily use of "+Math.round(dailyUseKw)+" kWh, you will need to purchase "+ panelsNeeded+ " "+ panelName+ " brand solar panels to save 100% of electricity bill.  </p>"
+feedback += "<h2>Additional Details</h2>"
+feedback += "<p>Your average daily electricity consumption: "+Math.round(dailyUseKw)+ "kwh per day</p>"
+feedback += "<p>Average sunshine hours per day: "+ sunHoursPerday+" hours</p>"
+feedback += "<p>Realisitc wants needed per hour: "+ Math.round(realWattNeeds)+ " watts/hour</p>"
+feedback += "<p>"+ panelName+" you selected genrates about "+panelOutput+ " watts per hour</p>"
+
+document.getElementById("feedback").innerHTML = feedback
+}
